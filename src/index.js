46666b423319737817e5523bcc5937bac4ae2a76
srimaharshi manchem <email2rishi13@gmail.com>
2019-06-01 11:45:43 +0530

    port issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const config = require('./config/config');
 const app = require('./config/express');
 // require('./config/mongoose');
 
-app.listen(config.port, () => {
+app.listen(config.port || 1920, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
 });
 
