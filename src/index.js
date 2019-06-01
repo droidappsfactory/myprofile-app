ac9c4c0b4679f04ac2135db401eba9a6dddca9d9
srimaharshi manchem <email2rishi13@gmail.com>
2019-06-01 11:55:48 +0530

    mongooose addded

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const config = require('./config/config');
 const app = require('./config/express');
-// require('./config/mongoose');
+require('./config/mongoose');
 
 app.listen(process.env.PORT || 5000, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
